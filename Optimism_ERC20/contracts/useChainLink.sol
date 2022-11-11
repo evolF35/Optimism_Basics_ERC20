b217c8f32e135bb02ff704bce542a3477066fbb6
evolF35 <maccyair@gmail.com>
2022-11-11 15:33:41 -0800

    we got an oracle working

--- Optimism_ERC20/contracts/useChainLink.sol
+++ Optimism_ERC20/contracts/useChainLink.sol
@@ -3,7 +3,21 @@ pragma solidity ^0.8.9;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract useChainkLink {
+contract useChainLink {
+    AggregatorV3Interface public priceFeed;
 
+    constructor() {
+        priceFeed = AggregatorV3Interface(0x57241A37733983F97C4Ab06448F244A1E0Ca0ba8);
+    }
+
+    function getLatestPrice() public view returns (int){
+        (,int price,,,) = priceFeed.latestRoundData();
+        return(price);
+    }
+
+    function getDecimals() public view returns(uint8){
+        uint8 decimals = priceFeed.decimals();
+        return(decimals);
+    }
 
 }
--- Optimism_ERC20/scripts/deploy.js
+++ Optimism_ERC20/scripts/deploy.js
@@ -3,12 +3,15 @@ const hre = require("hardhat");
 
 async function main() {
 
-  const Lock = await hre.ethers.getContractFactory("kilomanjaro");
-  const lock = await Lock.deploy(444343);
+  //const Lock = await hre.ethers.getContractFactory("kilomanjaro");
+  //const lock = await Lock.deploy(444343);
+
+  const Lock = await hre.ethers.getContractFactory("useChainLink");
+  const lock = await Lock.deploy();
 
   await lock.deployed();
 
-  console.log(`I think I broke something`);
+  console.log(`I think I broke something1,`,lock.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
